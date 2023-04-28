0c768c1680c7bfdc9c3b407cfe8bb86742fc3a4a
AlbinoLoaf <Martho2000@gmail.com>
2023-04-28 11:08:59 +0200

    Text

---
+++ frontend/src/components/Tweet.js
@@ -0,0 +1,13 @@
+import Card from 'react-bootstrap/Card';
+
+export default function Tweet(){
+    return (
+        <Card style={{width : '18rem'}}>
+                <Card.Title>Title </Card.Title>
+                <Card.Text> Lorem Ipsum</Card.Text>
+        </Card>
+
+    );
+
+    }
+
